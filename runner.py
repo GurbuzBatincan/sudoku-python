5a993a3e668710d10be112a3ec8ff70db1ccd44a
Andrew Carlson <acarl005@g.ucla.edu>
2015-05-09 16:45:33 -0700

    fix

--- runner.py
+++ runner.py
@@ -1,8 +1,8 @@
 from sudoku import *
 
 # f = open('set-01_sudoku_puzzles.txt', 'r')
-# f = open('set-02_project_euler_50-easy-puzzles.txt', 'r')
-f = open('set-03_peter-norvig_95-hard-puzzles.txt', 'r')
+f = open('set-02_project_euler_50-easy-puzzles.txt', 'r')
+# f = open('set-03_peter-norvig_95-hard-puzzles.txt', 'r')
 # f = open('set-04_peter-norvig_11-hardest-puzzles.txt', 'r')
 
 for i, line in enumerate(f):
